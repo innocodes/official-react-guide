0280b919d3cf7da103c93b5b0c4d0783afa53f48
innocodes <oyebodeinnocent@gmail.com>
2022-11-29 18:23:02 +0100

    create simple search table

--- src/App.js
+++ src/App.js
@@ -1,109 +1,124 @@
-import '../src/About.css'
 import {useState} from "react";
 
-const user = {
-    name: "Hedy Lamarr",
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
-}
-
-const products = [
-    {title: 'Cabbage', id: 1, isFruit: false },
-    {title: 'Garlic', id: 2, isFruit: true  },
-    {title: 'Apple', id: 3, isFruit: false  },
-]
-
-const listItems = products.map(product =>
-    // <li key={product.id}>{product.title}</li>
-    <li
-        key={product.id}
-        style={{
-            color: product.isFruit ? 'magenta' : 'darkgreen'
-        }}
-    >
-        {product.title}
-    </li>
-)
-
-function MyButton({ count, onClick }) {
+function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
 
     return (
-
         <div>
-            <button onClick={onClick}>Clicked {count} times</button>
-            {/*<ul>{listItems}</ul>*/}
-        </div>
-    );
-}
-
-function Profile() {
-    return (
-        <>
-            <h1>{user.name}</h1>
-            <img
-                className="avatar"
-                src={user.imageUrl}
-                alt={'Photo of ' + user.name}
-                style={{
-                    width: user.imageSize,
-                    height: user.imageSize
-                }}
+            <SearchBar
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly}
             />
-        </>
-    )
-}
-
-
-// Conditional Redenring
-let content;
-let isLoggedIn;
-if (isLoggedIn) {
-    content = <Profile />
-} else {
-    content = <MyButton />
-}
-
-export default function MyApp() {
-    const [count, setCount] = useState(0);
-
-    function handleClick() {
-        // alert('You clicked me!');
-        setCount(count + 1);
-    }
-    return (
-        <div>
-            <h1>Welcome to my app</h1>
-            <MyButton count={count} onClick={handleClick} />
-            <AboutPage />
-            <MyButton count={count} onClick={handleClick} />
-
-            <h1>
-                {user.name}
-            </h1>
-            <p>
-                {content}
-            </p>
-            <div>
-                {isLoggedIn ? (
-                    <MyButton />
-                ) : (
-                    <AboutPage />
-                )}
-            </div>
-            <div>
-                {isLoggedIn && <AboutPage />}
-            </div>
+            <ProductTable
+                products={products}
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+            />
         </div>
     );
 }
 
-function AboutPage() {
+function ProductCategoryRow({ category }) {
     return (
-        <>
-            <h1>About</h1>
-            <img className="avatar" />
-            <p className="avatar">Hello there. <br />How do you do?</p>
-            <p>Hello</p>
-        </>
-    )
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red'}}>
+            {product.name}
+        </span>;
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLocaleString()
+            ) === -1
+        ) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category}
+                    filterText={filterText}
+                />
+            );
+        }
+
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name}
+            />
+        );
+        lastCategory = product.category;
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function SearchBar({filterText, inStockOnly, onFilterTextChange}) {
+    return (
+        <form>
+            <input
+                type="text"
+                value={filterText}
+                placeholder="Search..."
+                onChange={(e) => onFilterTextChange(e.target.value)}
+            />
+            <label>
+                <input type="checkbox" checked={inStockOnly} />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+    return <FilterableProductTable products={PRODUCTS} />
 }
